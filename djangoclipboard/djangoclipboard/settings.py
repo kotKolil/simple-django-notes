dd9360b8154796bc78064ec176b558c2c9ccc48d
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 <ldxi1991@gmail.com>
2025-02-08 21:16:58 +0300

    added dockerfile

--- djangoclipboard/djangoclipboard/settings.py
+++ djangoclipboard/djangoclipboard/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mdp7dx6+)r4akk!og&ri^#)*#@7ob$eud#gg7oswbzxsz-u)z5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0"]
 
 
 # Application definition
